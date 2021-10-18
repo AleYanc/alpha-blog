d47cce15d6603889b6aafe117d85b43ab9fd137d
AleYanc <aleyanc2000@gmail.com>
2021-10-18 13:38:49 -0300

    Complete auth system

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery
+
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "../js/bootstrap_js_files.js";
+
 
 Rails.start();
 Turbolinks.start();
